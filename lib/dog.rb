f6ccb7a34b795df9d4d7ac53f5d6a020686af874
Dazha Brown <dajahbrown@hotmail.com>
2020-07-25 00:08:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,20 @@
 class Dog
+  attr_accessor :name
 
-	attr_accessor :name
+  @@all = []
 
-	@@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-	def initialize(name)
-		@name = name
-		@@all << self
-	end
+  def self.clear_all
+    @@all.clear
+  end
 
-	def self.clear_all
-		@@all.clear
-	end
-
-	def self.all
-		@@all.each { |dog_name| puts dog_name.name }
-	end
-
-
-end 
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
+  end
+end
